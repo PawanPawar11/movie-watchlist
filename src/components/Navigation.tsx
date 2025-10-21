4c5e689065e21b63986dec0b32b01cb77de5c0cd
PawanPawar11 <pawanpawar4369@gmail.com>
2025-10-21 19:11:43 +0530

    feat: create MovieListPage to fetch and render popular movies from TMDB API

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 const Navigation = () => {
   return (
     <>
-      <nav className="flex pb-2 gap-2 border-b-2 border-red-500">
+      <nav className="flex pb-4 mb-4 gap-4 border-b-2 border-red-500">
         <Link
           to="/"
           className="text-xl text-white bg-blue-500 px-6 py-2 rounded-sm cursor-pointer"
--- src/pages/MovieListPage.tsx
+++ src/pages/MovieListPage.tsx
@@ -1,5 +1,50 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const BASE_URL = "https://api.themoviedb.org/3";
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+interface Movie {
+  id: number;
+  title: string;
+  original_title: string;
+  poster_path: string;
+}
+
 const MovieListPage = () => {
-  return <div>MovieListPage</div>;
+  const [popularMovies, setPopularMovies] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    const fetchPopularMovies = async () => {
+      const { data } = await axios.get(
+        `${BASE_URL}/movie/popular?api_key=${API_KEY}`
+      );
+      console.log("Axios fetched data of popular movies: ", data);
+      setPopularMovies(data.results);
+    };
+
+    fetchPopularMovies();
+  }, []);
+  return (
+    <div className="grid grid-cols-5 gap-4">
+      {popularMovies.map((movie, id) => (
+        <div
+          key={id}
+          className="flex flex-col justify-center items-center rounded-sm gap-4 border-1"
+        >
+          {/* <p className="text-lg text-white">Title: {movie.title}</p> */}
+          <p className="text-lg text-white">
+            Original Title: {movie.original_title}
+          </p>
+          <img
+            className="w-40 h-60"
+            src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+            alt=""
+          />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default MovieListPage;
