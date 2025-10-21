4602a0f9e60484b83d7e6ca6c97a6c37cd91321b
PawanPawar11 <pawanpawar4369@gmail.com>
2025-10-21 18:38:38 +0530

    feat: Add navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Routes, Route } from "react-router";
+import MovieListPage from "./pages/MovieListPage";
+import WatchListPage from "./pages/WatchListPage";
+import Navigation from "./components/Navigation";
+
 const App = () => {
-  return <div className="p-4 bg-red-400">App</div>;
+  return (
+    <div className="p-4 bg-blue-950">
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<MovieListPage />} />
+        <Route path="/watchlist" element={<WatchListPage />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
