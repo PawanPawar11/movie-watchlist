8de1f6bb4a36c34673126a5971e55ad03c6a4924
PawanPawar11 <pawanpawar4369@gmail.com>
2025-10-22 13:47:05 +0530

    feat: Add local storage functionality to watchlist

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,21 @@ import { Routes, Route } from "react-router";
 import MovieListPage, { type Movie } from "./pages/MovieListPage";
 import WatchListPage from "./pages/WatchListPage";
 import Navigation from "./components/Navigation";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const App = () => {
   const [watchListItems, setWatchListItems] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    const storedWatchList = localStorage.getItem("watchlist");
+    if (storedWatchList) {
+      setWatchListItems(JSON.parse(storedWatchList));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("watchlist", JSON.stringify(watchListItems));
+  }, [watchListItems]);
 
   return (
     <div className="p-4 bg-blue-950 min-h-screen">
